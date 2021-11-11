50b8079224f10f711b024793c08581a48ecb1c36
Nicolas Eugui <nico2014.gta@gmail.com>
2021-11-11 14:02:36 -0300

    optimizacion de imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 
 // Imagenes
-
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 
 function css(done) {
@@ -12,6 +13,17 @@ function css(done) {
         .pipe(sass()) // Compilarlo
         .pipe(dest('build/css')) // Almacenarla en el Disco Duro
     done();
+}
+
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/**')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'))
+
+    done()
 }
 
 function versionWebp (done) {
@@ -30,5 +42,6 @@ function dev(done) {
 }
 
 exports.css = css;
+exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(versionWebp, dev);
+exports.dev = parallel(imagenes, versionWebp, dev);
