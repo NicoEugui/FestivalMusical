89ed66481b43667edf57eb9a2e9b8d743cfed5c3
Nicolas Eugui <nico2014.gta@gmail.com>
2021-11-11 18:21:04 -0300

    +js

--- build/js/app.js
+++ build/js/app.js
@@ -60,3 +60,17 @@ function mostrarImagen(id) {
     body.appendChild(overlay);
     body.classList.add('fijar-body');
 }
+
+function scrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+    
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click', function(e){
+            e.preventDefault();
+
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({behavior: "smooth"});
+        });
+    });
+}
