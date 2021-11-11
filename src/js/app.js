ef14165027e5861d62e3af5692170fedffb300ef
Nicolas Eugui <nico2014.gta@gmail.com>
2021-11-11 17:31:21 -0300

    seleccionar imagenes terminado

--- src/js/app.js
+++ src/js/app.js
@@ -16,6 +16,47 @@ function crearGaleria() {
             <source srcset="build/img/thumb/${i}.webp" type="image/webp">
             <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="Imagen Galeria">
         `;
+
+        imagen.onclick = function() {
+            mostrarImagen(i);
+        }
+
+        galeria.appendChild(imagen)
+
         galeria.appendChild(imagen);
     }
+}
+
+function mostrarImagen(id) {
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = 
+    `
+        <source srcset="build/img/grande/${id}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="Imagen Galeria">
+    `;
+    // Crea el overlay con la imagen
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+    overlay.onclick = function() {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
+    
+    // Boton para cerrar el imagen
+    const cerrarImagen = document.createElement('P');
+    cerrarImagen.textContent = 'X';
+    cerrarImagen.classList.add('btn-cerrar');
+    cerrarImagen.onclick = function() {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
+    overlay.appendChild(cerrarImagen);
+
+    // Add al html
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    body.classList.add('fijar-body');
 }
